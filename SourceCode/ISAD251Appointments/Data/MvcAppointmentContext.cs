c2d1a5d11f1dd4fd1c742b94e5d12f5899c738de
Cryda <jayrees9@hotmail.com>
2020-08-24 15:29:42 +0100

    Recreate
    Remade the project based on a new sql. Changed the interactions a user can have on their respected page based on the user requirements.  Overall took 9 hours

---
+++ SourceCode/ISAD251Appointments/Data/MvcAppointmentContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ISAD251Appointments.Models;
+
+
+namespace ISAD251Appointments.Data
+{
+    public class MvcAppointmentContext : DbContext
+    {
+        public MvcAppointmentContext(DbContextOptions<MvcAppointmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Appointments> Appointments { get; set; }
+    
+    }
+}
