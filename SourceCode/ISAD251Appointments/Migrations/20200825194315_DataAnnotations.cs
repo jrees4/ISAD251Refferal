e78813083b3ff9faf790b0b1bdaada1593563f0c
Cryda <jayrees9@hotmail.com>
2020-08-25 23:40:37 +0100

    Upade
    Add required stuff

---
+++ SourceCode/ISAD251Appointments/Migrations/20200825194315_DataAnnotations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ISAD251Appointments.Migrations
+{
+    public partial class DataAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppointmentName = table.Column<string>(maxLength: 100, nullable: false),
+                    AppointmentDescription = table.Column<string>(maxLength: 255, nullable: true),
+                    AppointmentTarget = table.Column<string>(maxLength: 255, nullable: false),
+                    AppointmentTime = table.Column<DateTime>(nullable: false),
+                    AppointmentNote = table.Column<string>(maxLength: 400, nullable: true),
+                    AppointmentIsComplete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+        }
+    }
+}
---
+++ SourceCode/ISAD251Appointments/Models/Appointments.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace ISAD251Appointments.Models
+{
+    public class Appointments
+    {
+        [Key] public int AppointmentID { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Display(Name = "Appointment")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string AppointmentName { get; set; }
+
+        [Display(Name = "Description")]
+        [StringLength(255, MinimumLength = 1)]
+        public string AppointmentDescription { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Display(Name = "Family Member")]
+        [StringLength(255, MinimumLength = 1)]
+        [Required]
+        public string AppointmentTarget { get; set; }
+
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime AppointmentTime { get; set; }
+
+
+        [Display(Name = "Note")]
+        [StringLength(400, MinimumLength = 0)]
+        public string AppointmentNote { get; set; }
+
+
+        [Display(Name = "Completed")]
+        public bool AppointmentIsComplete { get; set; }
+
+    }
+    }
+
